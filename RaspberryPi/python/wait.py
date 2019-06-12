f6f2d2fecb45a864401cb82e25954cad01b94e60
BriannaLi <14zl630@gmail.com>
2019-06-12 17:05:15 -0400

    read json file

--- RaspberryPi/python/wait.py
+++ RaspberryPi/python/wait.py
@@ -5,72 +5,23 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 from PIL import ImageColor
+from urllib.request import urlopen
+import json
+from pprint import pprint
 from gpiozero import Button
 #from Tkinter import *
 
-'''
-def gif():
-    canvas = Canvas(width = 300, height = 200, bg = 'yellow')
-    canvas.pack(expand = YES, fill = BOTH)
-    gif1 = PhotoImage(file = 'DrBunsen.gif')
-    canvas.create_image(50, 10, image = gif1, anchor = NW)
-    mainloop()
-return
-'''
 font = ImageFont.truetype("ASMAN.TTF", 15)
 buttonDown = Button("BCM19")
 buttonUp = Button("BCM6")
 
-def ifRunning(draw,contaner,location):
-    if contaner==1:
-        draw.text((77, location), 'Running', fill = "GREEN", font=font)
-    elif contaner==2:
-        draw.text((77, location), 'Loading', fill = "Orange", font=font)
-    else:
-        draw.text((77, location), 'Stop', fill = "Red", font=font)
-    return
-
-def ifRunningDot(draw,contaner,location):
-    if contaner==1:
+def ifRunningDot(draw,state,location):
+    if state=="UP":
         draw.ellipse((96,location-1,112,location+15),fill = "Green",outline = "Green")
-    elif contaner==2:
-        draw.ellipse((96,location-1,112,location+15),fill = "Orange",outline = "Orange")
+    elif state=="LOAD":
+        draw.ellipse((98,location+1,110,location+13),fill = "Orange",outline = "Orange")
     else:
-        draw.ellipse((96,location-1,112,location+15),fill = "Red",outline = "Red")
-    return
-
-def status(LCD,A,G,L,N,M):
-    print ("***show container status")
-    image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
-    draw = ImageDraw.Draw(image)
-    draw.text((2, 12), 'Admin:', fill = "WHITE", font=font)
-    ifRunning(draw,A,12)
-    draw.text((2, 37), 'Game:', fill = "WHITE", font=font)
-    ifRunning(draw,G,37)
-    draw.text((2, 62), 'Leaderboard:', fill = "WHITE", font=font)
-    ifRunning(draw,L,62)
-    draw.text((2, 87), 'nginx:', fill = "WHITE", font=font)
-    ifRunning(draw,N,87)
-    draw.text((2, 112), 'mango:', fill = "WHITE", font=font)
-    ifRunning(draw,M,112)
-    LCD.LCD_ShowImage(image,0,0)
-    return
-
-def statusDot(LCD,A,G,L,N,M):
-    print ("***show container status")
-    image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
-    draw = ImageDraw.Draw(image)
-    draw.text((4, 12), 'Admin:', fill = "WHITE", font=font)
-    ifRunningDot(draw,A,12)
-    draw.text((4, 37), 'Game:', fill = "WHITE", font=font)
-    ifRunningDot(draw,G,37)
-    draw.text((4, 62), 'Leaderboard:', fill = "WHITE", font=font)
-    ifRunningDot(draw,L,62)
-    draw.text((4, 87), 'nginx:', fill = "WHITE", font=font)
-    ifRunningDot(draw,N,87)
-    draw.text((4, 112), 'mango:', fill = "WHITE", font=font)
-    ifRunningDot(draw,M,112)
-    LCD.LCD_ShowImage(image,0,0)
+        draw.ellipse((100,location+3,108,location+11),fill = "Red",outline = "Red")
     return
 
 #try:
@@ -81,18 +32,36 @@ def main():
     Lcd_ScanDir = LCD_1in44.SCAN_DIR_DFT  #SCAN_DIR_DFT = D2U_L2R
     LCD.LCD_Init(Lcd_ScanDir)
     
+    #url = urlopen('')
+    #data = json.loads(url.read().decode('utf-8'))
+    
+    with open('sample.json') as f:
+        data = json.load(f)
+    pprint(data)
+    i=0
+    location = 12
     while True:
-        status(LCD,1,2,1,0,1)
+        image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+        draw = ImageDraw.Draw(image)
+        while i<len(data.get('checks')):
+            draw.text((4, location), data.get('checks')[i].get('name'), fill = "WHITE", font=font)
+            ifRunningDot(draw,data.get('checks')[i].get('state'),location)
+            i += 1
+            location += 25
+            if location>128:
+                break
+        LCD.LCD_ShowImage(image,0,0)
+        LCD_Config.Driver_Delay_ms(1000)
+        if location<128:
+            i=0
+        location = 12
+        
         #LCD_Config.Driver_Delay_ms(10000)
+        '''
         while True:
             if buttonDown.is_pressed:
                 break
-    
-        statusDot(LCD,1,2,1,0,1)
-        while True:
-            if buttonUp.is_pressed:
-                break
-    
+                '''
         #while (True):
     
 if __name__ == '__main__':
