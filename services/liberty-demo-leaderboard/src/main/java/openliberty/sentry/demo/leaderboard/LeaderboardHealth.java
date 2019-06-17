0a4f8ba6c9abef29a70c380502906af251c20925
Jiwoo-Lim <jiwoo.lim@ibm.com>
2019-06-17 14:42:12 -0400

    leaderboard health service

--- services/liberty-demo-leaderboard/src/main/java/openliberty/sentry/demo/leaderboard/LeaderboardHealth.java
+++ services/liberty-demo-leaderboard/src/main/java/openliberty/sentry/demo/leaderboard/LeaderboardHealth.java
@@ -5,21 +5,53 @@ import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 
+import java.net.URI;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
 
 @Health
 @ApplicationScoped
 public class LeaderboardHealth implements HealthCheck {
 
+	public boolean isHealthy() {
+		try {
+			String url = null;
+			
+		      try {
+		      URI uri = new URI("http", null, "localhost", Integer.parseInt(System.getProperty("default.http.port")), "/liberty-demo-leaderboard/app/leaderboard/top_scores", null, null);
+		      
+		      url = uri.toString();
+		      System.out.println(url);
+		      }catch (Exception e) {
+		      System.out.println("URISyntaxException");
+		      }
+
+		      Client client = ClientBuilder.newClient();
+		      Response response = client.target(url).request(MediaType.APPLICATION_JSON)
+		                                .get();
+		      if (response.getStatus() != 200) {
+		        return false;
+		      }
+		      return true;
+		    } catch (Exception e) {
+		      return false;
+		    }
+	}
+	
+
   @Override
   public HealthCheckResponse call() {
-  if (!LeaderboardResource.IS_CONNECTED) {
+  if (!isHealthy()) {
     return HealthCheckResponse.named(LeaderboardHealth.class.getSimpleName())
-                              .withData("MongoDB", "not available").down()
+                              .withData("LeaderboardHealth", "not available").down()
                               .build();
   }
   return HealthCheckResponse.named(LeaderboardHealth.class.getSimpleName())
            
-                            .withData("MongoDB", "available").up().build();
+                            .withData("LeaderboardHealth", "available").up().build();
   }
   
 
