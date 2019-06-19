dd8517d089e70d5ffd65d04dff0448e0f5d5aadf
BriannaLi <14zl630@gmail.com>
2019-06-19 18:13:39 -0400

    get plot data

--- RaspberryPi/python/matrics.py
+++ RaspberryPi/python/matrics.py
@@ -9,6 +9,10 @@ from urllib.request import urlopen
 import json
 from pprint import pprint
 from gpiozero import Button
+from prometheus_client.parser import text_string_to_metric_families
+import requests
+from requests.auth import HTTPBasicAuth
+import os
 #from Tkinter import *
 
 font = ImageFont.truetype("Open.ttf", 11)
@@ -16,7 +20,7 @@ font2 = ImageFont.truetype("Open.ttf", 22)
 buttonDown = Button("BCM19")
 buttonUp = Button("BCM6")
 
-def ifRunningDot(draw,state,location):
+def plot(draw,state,location):
     if state=="UP":
         draw.ellipse((104,location-1,120,location+15),fill = "Green",outline = "Green")
     #elif state=="LOAD":
@@ -26,7 +30,7 @@ def ifRunningDot(draw,state,location):
     return
 
 def noUrl(draw):
-    draw.text((10, 30), "No service", fill = "WHITE", font=font2)
+    draw.text((10, 30), "No metrics", fill = "WHITE", font=font2)
     draw.text((15, 70), "Avaliable", fill = "WHITE", font=font2)
     
 #try:
@@ -43,40 +47,27 @@ def main():
     #with open('sample.json') as f:
     #    data = json.load(f)
     #pprint(data)
-    i=0
-    location = 12
-    while True:
-        try:
-            url = urlopen('http://localhost:9080/health')
-            data = json.loads(url.read().decode('utf-8'))
-            image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
-            draw = ImageDraw.Draw(image)
-            while i<len(data.get('checks')):
-                draw.text((4, location), data.get('checks')[i].get('name'), fill = "WHITE", font=font)
-                ifRunningDot(draw,data.get('checks')[i].get('state'),location)
-                i += 1
-                location += 25
-                if location>128:
-                    break
-            LCD.LCD_ShowImage(image,0,0)
-            LCD_Config.Driver_Delay_ms(1000)
-            if location<128:
-                i=0
-            location = 12
-        except:
-            image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
-            draw = ImageDraw.Draw(image)
-            noUrl(draw)
-            LCD.LCD_ShowImage(image,0,0)
-            LCD_Config.Driver_Delay_ms(1000)
-        
+    #while True:
+        #try:
+        #metrics = urlopen('http://localhost:9080/metrics')
+    os.system("./metrics.sh")
+    with open('metrics.json') as f:
+        plotdata = json.load(f)
+    pprint(plotdata)
+    print(plotdata.get('base').get('memory.usedHeap'))
+        #plotdata = metrics.read()
+        #print(type(plotdata))
+    image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+    draw = ImageDraw.Draw(image)
+
+        #except:
+        #    image = Image.new("RGB", (LCD.width, LCD.height), "BLACK")
+        #    draw = ImageDraw.Draw(image)
+        #    noUrl(draw)
+        #    LCD.LCD_ShowImage(image,0,0)
+        #    LCD_Config.Driver_Delay_ms(1000)
+            
         #LCD_Config.Driver_Delay_ms(10000)
-        '''
-        while True:
-            if buttonDown.is_pressed:
-                break
-                '''
-        #while (True):
     
 if __name__ == '__main__':
     main()
