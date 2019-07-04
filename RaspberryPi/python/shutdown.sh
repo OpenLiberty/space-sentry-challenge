bf425666283684e7f70e8145f289e42bf4aab5a3
BriannaLi <14zl630@gmail.com>
2019-07-04 16:17:31 -0400

    cd

--- RaspberryPi/python/shutdown.sh
+++ RaspberryPi/python/shutdown.sh
@@ -3,3 +3,4 @@ cd /home/pi/brianna/guide-microprofile-metrics/finish && mvn liberty:stop-server
 cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
 pkill -f switch.py
 sudo docker stop space_s
+sudo docker rm space_s
--- RaspberryPi/python/startup.sh
+++ RaspberryPi/python/startup.sh
@@ -1,8 +1,10 @@
+cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
 chmod +x welcome.py
 python3 welcome.py
+sudo docker rm space_s
 sudo docker run -i -t -d --name space_s rasp-liberty:latest
 cd /home/pi/brianna/guide-microprofile-health/finish && mvn liberty:start-server
 cd /home/pi/brianna/guide-microprofile-metrics/finish && mvn install liberty:start-server
 cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
 chmod +x switch.py
-nohup python3 switch.py > outputswitch.log &
+nohup python3 switch.py >> outputswitch.log &
