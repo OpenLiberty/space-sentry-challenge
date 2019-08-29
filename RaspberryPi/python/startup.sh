fea3b89768e445d3efac324940ac77719ab98507
Brianna Li <32473795+BriannaLi@users.noreply.github.com>
2019-08-29 13:30:12 -0400

    Update startup.sh

--- RaspberryPi/python/startup.sh
+++ RaspberryPi/python/startup.sh
@@ -1,7 +1,7 @@
 cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
 chmod +x welcome.py
 python3 welcome.py
-sudo /home/pi/brianna/git/space-sentry-challenge/services/start.sh
+#sudo /home/pi/brianna/git/space-sentry-challenge/services/start.sh
 cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
 chmod +x switch.py
 nohup python3 switch.py >> outputswitch.log &
