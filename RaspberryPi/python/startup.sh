aa2d4a2e8750fa25dc4d6fedf0ea0f5217200fe8
Frank Ji <31771273+fwji@users.noreply.github.com>
2019-09-05 15:33:37 -0400

    Update startup.sh

--- RaspberryPi/python/startup.sh
+++ RaspberryPi/python/startup.sh
@@ -1,7 +1,4 @@
-cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
-chmod +x welcome.py
-python3 welcome.py
-#sudo /home/pi/brianna/git/space-sentry-challenge/services/start.sh
-cd /home/pi/brianna/git/space-sentry-challenge/RaspberryPi/python
+cd /home/pi/space-sentry-challenge/RaspberryPi/python
 chmod +x switch.py
-nohup python3 switch.py >> outputswitch.log &
+nohup python switch.py >> outputswitch.log &
+
